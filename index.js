aa65cb7ff1a538fa6b16686cc62e142229b75391
md-mostafa <md.mostafa.akash@gmail.com>
2021-06-15 00:16:39 +0600

    connecting to mongodb atlas

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ import cors from 'cors'; //cros origin resource sharing.
                         //which is normally prohibited because of SOP
 
 
+const app = express();
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json({ limit: "30mb", extended: true}));
+
+app.use(cors());
+
+//connecting to mongodb
+const CONNECTION_URL = 'mongodb+srv://mdmostafa:mdmostafa@nodejs-authentication.4tkhl.mongodb.net/test?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+//using mongoose to connect to our database
+mongoose.connect(CONNECTION_URL, { 
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+})
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+
+mongoose.set('useFindAndModify', false);
