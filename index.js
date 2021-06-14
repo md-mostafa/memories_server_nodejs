4008ca56cdc58a698df65876c9c16ff8f4438ccd
md-mostafa <md.mostafa.akash@gmail.com>
2021-06-14 20:57:59 +0600

    installed dependencies

---
+++ index.js
@@ -0,0 +1,9 @@
+//imorting the dependencies
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors'; //cros origin resource sharing. 
+                        //allows to make request from one website to another website in the browser,
+                        //which is normally prohibited because of SOP
+
+
