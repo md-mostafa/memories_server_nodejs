b5a441ef02eac7b3a86b68723bb2940a91ebd4e3
md-mostafa <md.mostafa.akash@gmail.com>
2021-07-01 02:47:41 +0600

    adding .env.example to github

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 //connecting to mongodb
-//const CONNECTION_URL = 'mongodb+srv://mdmostafa:mdmostafa@nodejs-authentication.4tkhl.mongodb.net/test?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
 //using mongoose to connect to our database
