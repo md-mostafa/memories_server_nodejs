cfc972f77f01539465f49431087cd2259677e35f
md-mostafa <md.mostafa.akash@gmail.com>
2021-07-01 02:43:46 +0600

    environment variable

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import cors from 'cors'; //cros origin resource sharing. 
                         //allows to make request from one website to another website in the browser,
                         //which is normally prohibited because of SOP
@@ -10,6 +11,7 @@ import cors from 'cors'; //cros origin resource sharing.
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json({ limit: "30mb", extended: true}));
@@ -19,11 +21,11 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 //connecting to mongodb
-const CONNECTION_URL = 'mongodb+srv://mdmostafa:mdmostafa@nodejs-authentication.4tkhl.mongodb.net/test?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://mdmostafa:mdmostafa@nodejs-authentication.4tkhl.mongodb.net/test?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 //using mongoose to connect to our database
-mongoose.connect(CONNECTION_URL, { 
+mongoose.connect(process.env.CONNECTION_URL, { 
     useNewUrlParser : true,
     useUnifiedTopology: true
 })
