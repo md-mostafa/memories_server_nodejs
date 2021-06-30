ee6d4138edb944b1bec762ce8c5636559b2ff145
md-mostafa <md.mostafa.akash@gmail.com>
2021-07-01 01:30:09 +0600

    like functionality

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/post.js';
+import { getPosts, createPost, updatePost, deletePost,likePost } from '../controllers/post.js';
 
 const router = express.Router();
 
@@ -15,6 +15,7 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 
 export default router;
