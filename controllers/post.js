59ad4128fd74811a6ca021cd158390c913299167
md-mostafa <md.mostafa.akash@gmail.com>
2021-07-20 23:24:15 +0600

    pagination

--- controllers/post.js
+++ controllers/post.js
@@ -2,13 +2,20 @@ import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
+    const { page } = req.query;
     try {
+
+        const LIMIT = 8;
+        const startIndex = (Number(page) - 1) * LIMIT;  //GET the start index of every page
+        const total = await PostMessage.countDocuments({});
+
         //this is an asynchronus action
         //finding something inside model takes time
-        const postMessages = await PostMessage.find(); 
+        ///const postMessages = await PostMessage.find(); 
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex); //this will give us newest post first since we are sorting them bases on id
         //console.log(postMessages);
 
-        res.status(200).json(postMessages);
+        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
     }catch(error){
         res.status(404).json({ message: error.message });
     }
